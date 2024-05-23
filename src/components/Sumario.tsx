04be8da6592921b9bc5208350a3ea2b4549ecb08
msouza28 <matheus.emiliano28@gmail.com>
2024-05-23 14:12:06 -0300

    Arrumando a frase do sumario nenhuma denuncia

--- src/components/Sumario.tsx
+++ src/components/Sumario.tsx
@@ -5,7 +5,7 @@ export default function Sumario() {
         <div className="flex pt-6 sm:pt-3 justify-center">
           <div  className="flex mx-4 sm:mx-0 items-center gap-2">
           <span className={`h-3 w-3 rounded-full bg-white`}></span>
-          <span className="text-whiteCustom text-sm">Nehuma denúncia</span>
+          <span className="text-whiteCustom text-sm">Nenhuma denúncia</span>
 
           <span className={`h-3 w-3 rounded-full bg-yellow-300`}></span>
           <span className="text-whiteCustom text-sm">Entre 1 a 3 denúncias</span>
