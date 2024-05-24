7caac825f49cfdb91b01b07f23c138a883f8e0ac
msouza28 <matheus.emiliano28@gmail.com>
2024-05-24 14:29:03 -0300

    Ajuste na responsividade do sumario

--- src/components/Sumario.tsx
+++ src/components/Sumario.tsx
@@ -3,18 +3,18 @@ export default function Sumario() {
 
     return (
         <div className="flex pt-6 sm:pt-3 justify-center">
-          <div  className="flex mx-4 sm:mx-0 items-center gap-2">
+          <div  className="flex mx-4 sm:mx-0 items-center gap-1 sm:gap-2">
           <span className={`h-3 w-3 rounded-full bg-white`}></span>
-          <span className="text-whiteCustom text-sm">Nenhuma denúncia</span>
+          <span className="text-whiteCustom text-xs sm:text-sm">Nenhuma denúncia</span>
 
           <span className={`h-3 w-3 rounded-full bg-yellow-300`}></span>
-          <span className="text-whiteCustom text-sm">Entre 1 a 9 denúncias</span>
+          <span className="text-whiteCustom text-xs sm:text-sm">Entre 1 a 9 denúncias</span>
 
           <span className={`h-3 w-3 rounded-full bg-orange-600`}></span>
-          <span className="text-whiteCustom text-sm">Entre 10 a 19 denúncias</span>
+          <span className="text-whiteCustom text-xs sm:text-sm">Entre 10 a 19 denúncias</span>
 
           <span className={`h-3 w-3 rounded-full bg-red-600`}></span>
-          <span className="text-whiteCustom text-sm">Acima de 20 denúncias</span>
+          <span className="text-whiteCustom text-xs sm:text-sm">Acima de 20 denúncias</span>
           </div>
         </div>
       );
